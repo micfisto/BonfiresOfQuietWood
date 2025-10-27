691e82be58f199c09fb96673a3f9547a2b0a9d5f
Alesya Terlikova <micfisto@gmail.com>
2025-10-27 13:38:02 +0300

    Refactor: Separate PlayerController and GameInput; removed old input actions; added new PlayerInputActions and PlayerControls

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,6 +1,21 @@
+using System;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
-public class PlayerController: MonoBehaviour
+public class PlayerController : MonoBehaviour
 {
+    [SerializeField] private float movingSpeed = 5f;
+    private Rigidbody2D rigidbody;
     
+    private void Awake()
+    {
+        rigidbody = GetComponent<Rigidbody2D>();
+    }
+
+    private void FixedUpdate()
+    {
+        Vector2 inputVector = GameInput.Instance.GetMovementVector();
+        inputVector = inputVector.normalized;
+        rigidbody.MovePosition(rigidbody.position + inputVector * (movingSpeed * Time.fixedDeltaTime));
+    }
 }
